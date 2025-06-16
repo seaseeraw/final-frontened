29298f93e570c2ae1c7e2dbeb44251174c136946
Shishir Pathak <seaseeraw12@gmail.com>
2025-06-16 16:08:18 +0545

    hey

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../App.css'
 const Navbar = () => {
   return (
-    
-      <div className='navs'>
-        <div className='logo'>Logo</div>
-        <div className='search'>
-          <input type="text" placeholder='Search' />
-          <button id='a'>Search</button>
-        </div>
-        <div className='login'>Login</div>
-      </div>
-       
-
-  
-    
+    <div className='navs'>
+      <div className='logo'><img src="./src/assets/logo.jpg" alt="Logo" width="80" height="50"/></div>
+      {/* <div className='search'>
+        <input type="text" placeholder='Search' />
+        <button id='a'>Search</button>
+      </div> */}
+      <div className='login'><Link to="/login">Login</Link></div>
+    </div>
   )
 }
 
