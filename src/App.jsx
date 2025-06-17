e691f94e27202ab329868998430a0e5ea41d3b5f
Shishir Pathak <seaseeraw12@gmail.com>
2025-06-17 19:22:29 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/about';
 import Contact from './pages/contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   return (
    
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../App.css';
+import Button from 'react-bootstrap/Button';
 const Home = () => {
   return (
     <>
     <div className='home'>
       <div className='box'><h1>Welcome to the Home Page</h1></div>
-      <div className='box'><p>This is the home page of our website.</p></div>
+      <div className='box'><p>This is the home page of our website.</p><br/><Button variant="success">Success</Button></div>
       <div className='box'><p>Here you can find various resources and information.</p></div>
     </div>
     </>
