8ee45c894144d1f6a908054e5acd78385e5f53d9
Shishir Pathak <seaseeraw12@gmail.com>
2025-07-02 19:08:09 +0545

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   return (
     <div className='nav'>
       <img src="./src/assets/logo.jpg" width="40" height="40" style={{ borderRadius: "5px" }} alt="Logo" />
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <div className="shi">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,24 @@
-// import React from 'react'
-
-import React, { useState } from 'react';
+import React from 'react'
 import '../App.css';
-
 const Home = () => {
-  const[message, setmessage]=useState("Hello World");
-  const handleOnClick = () => {
-    setmessage("Button clicked!");
-  };
-
   return (
     <>
       <div className='a'>
         <section id='b'>
           Dashboard
-          <ul>
-            <li onClick={handleOnClick}>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
+          <section id='k'><i class="fa-brands fa-react"></i>1</section>
+          <section id='k'>2</section>
+          <section id='k'>3</section>
+          <section id='k'>4</section>   
+          <section id='k'>5</section>
+          <section id='k'>6</section>
         </section>
-
-        <section id='c'>{message}</section>
+        <section id='c'>2</section>
+        <section id='d'>3</section>
       </div>
     </>
-  );
-};
+  )
+}
 
 export default Home;
 
-// import '../App.css';
-// const Home = () => {
-//   return (
-    
-//     <>
-//     const handleOnClick = () => {
-//   console.log("Button clicked!");
-// };
-
-//     <div className='a'>
-//       <section id='b'>Dashboard
-//         <li>Item 1 {Home}</li>
-//         <li>Item 2 </li>
-//         <li>Item 3 </li>
-//       </section>
-//       <section id='c'>World</section>
-      
-//     </div>
-
-   
-//     </>
-//   )
-// }
-
-// export default Home;
