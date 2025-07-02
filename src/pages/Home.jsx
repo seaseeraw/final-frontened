b5e87334b56399b95909585a2c27ed00f56fb8f5
Shishir Pathak <seaseeraw12@gmail.com>
2025-07-02 19:49:59 +0545

    hey

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,13 @@ const Home = () => {
       <div className='a'>
         <section id='b'>
           Dashboard
-          <section id='k'><i class="fa-brands fa-react"></i>1</section>
-          <section id='k'>2</section>
-          <section id='k'>3</section>
-          <section id='k'>4</section>   
-          <section id='k'>5</section>
-          <section id='k'>6</section>
+          
+          <section id='k'><i class="fa-brands fa-react" ></i> Investment</section>
+  <section id='k'><i class="fa-regular fa-chart-bar" ></i> Analysis</section>
+  <section id='k'><i class="fa-regular fa-user" ></i> Admin/HR</section>
+  <section id='k'><i class="fa-solid fa-chart-line" ></i> Sales</section>
+  <section id='k'><i class="fa-regular fa-handshake" ></i> Product</section>
+  <section id='k'><i class="fa-solid fa-coins" ></i> Transition</section>
         </section>
         <section id='c'>2</section>
         <section id='d'>3</section>
